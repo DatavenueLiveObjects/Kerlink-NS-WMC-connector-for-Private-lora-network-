a55aa5d3f587286508ddb007513c5859f76699d7
grzegorzswistak <grzegorz.swistak2@gmail.com>
2022-09-15 13:39:53 +0200

    Counters and health

---
+++ src/main/java/com/orange/lo/sample/kerlink2lo/utils/CountersActuatorEndpoint.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Orange. All Rights Reserved.
+ * <p>
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.orange.lo.sample.kerlink2lo.utils;
+
+import io.micrometer.core.instrument.Counter;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Endpoint(id = "counters")
+public class CountersActuatorEndpoint {
+
+  private final Counters counters;
+
+  public CountersActuatorEndpoint(Counters counters) {
+    this.counters = counters;
+  }
+
+  @ReadOperation
+  public Map<String, Long> getCountersStatus() {
+    HashMap<String, Long> countersStatus = new HashMap<>();
+    counters.getAll().forEach(c -> countersStatus.put(c.getId().getName(), val(c)));
+    return countersStatus;
+  }
+  
+  private long val(Counter cnt) {
+      return Math.round(cnt.count());
+  }
+}
--- src/test/java/com/orange/lo/sample/kerlink2lo/Kerlink2LoControllerTest.java
+++ src/test/java/com/orange/lo/sample/kerlink2lo/Kerlink2LoControllerTest.java
@@ -4,41 +4,87 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.orange.lo.sample.kerlink2lo.kerlink.model.DataDownEventDto;
 import com.orange.lo.sample.kerlink2lo.kerlink.model.DataUpDto;
 import com.orange.lo.sample.kerlink2lo.lo.LoApiExternalConnectorService;
+import com.orange.lo.sample.kerlink2lo.utils.Counters;
+import io.micrometer.core.instrument.Counter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpHeaders;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class Kerlink2LoControllerTest {
     private static final String KERLINK_ACCOUNT_HEADER = "Kerlink-Account";
 
     private Kerlink2LoController kerlink2LoController;
+    @Mock
+    private Counters counters;
+    @Mock
+    private LoApiExternalConnectorService externalConnectorService;
 
     @BeforeEach
     private void setUp() {
-        LoApiExternalConnectorService externalConnectorService = Mockito.mock(LoApiExternalConnectorService.class);
-        kerlink2LoController = new Kerlink2LoController(externalConnectorService);
+        kerlink2LoController = new Kerlink2LoController(externalConnectorService, counters);
     }
 
     @Test
-    void dataUp() throws Exception {
+    void shouldSendMessageAndIncrementCountersWhenDataUpReceivesMessageAndAccountHeader() throws Exception {
         DataUpDto dataUpDto = buildExampleDataUpDto();
         HttpHeaders headers = new HttpHeaders();
         headers.add(KERLINK_ACCOUNT_HEADER, "acc");
+        Counter sentAttemptCounter = Mockito.mock(Counter.class);
+        when(counters.getMessageSentAttemptCounter()).thenReturn(sentAttemptCounter);
+        Counter messageReadCounter = Mockito.mock(Counter.class);
+        when(counters.getMessageReadCounter()).thenReturn(messageReadCounter);
+
         kerlink2LoController.dataUp(dataUpDto, headers).call();
+
+        Mockito.verify(externalConnectorService, times(1)).sendMessage(dataUpDto, "acc");
+        Mockito.verify(messageReadCounter, times(1)).increment();
+        Mockito.verify(sentAttemptCounter, times(1)).increment();
+    }
+
+    @Test
+    void shouldNotSendMessageAndIncrementCountersWhenDataUpReceivesMessageAndNoAccountHeader() throws Exception {
+        DataUpDto dataUpDto = buildExampleDataUpDto();
+        HttpHeaders headers = new HttpHeaders();
+        Counter sentAttemptCounter = Mockito.mock(Counter.class);
+        when(counters.getMessageSentAttemptCounter()).thenReturn(sentAttemptCounter);
+        Counter messageReadCounter = Mockito.mock(Counter.class);
+        when(counters.getMessageReadCounter()).thenReturn(messageReadCounter);
+        Counter messageSentAttemptFailedCounter = Mockito.mock(Counter.class);
+        when(counters.getMessageSentAttemptFailedCounter()).thenReturn(messageSentAttemptFailedCounter);
+        Counter messageSentFailedCounter = Mockito.mock(Counter.class);
+        when(counters.getMessageSentFailedCounter()).thenReturn(messageSentFailedCounter);
+
+        kerlink2LoController.dataUp(dataUpDto, headers).call();
+
+        Mockito.verify(externalConnectorService, times(0)).sendMessage(any(), any());
+        Mockito.verify(messageReadCounter, times(1)).increment();
+        Mockito.verify(sentAttemptCounter, times(1)).increment();
+        Mockito.verify(messageSentAttemptFailedCounter, times(1)).increment();
+        Mockito.verify(messageSentFailedCounter, times(1)).increment();
     }
 
     @Test
     void dataDown() throws Exception {
-        // TODO: Example of proper DTO here
         DataDownEventDto dataDownDto = new DataDownEventDto();
+        dataDownDto.setStatus("OK");
         HttpHeaders headers = new HttpHeaders();
         headers.add(KERLINK_ACCOUNT_HEADER, "acc");
         kerlink2LoController.dataDown(dataDownDto, headers).call();
+
+        Mockito.verify(externalConnectorService, times(1)).sendCommandResponse(dataDownDto);
     }
 
     private DataUpDto buildExampleDataUpDto() throws IOException {
