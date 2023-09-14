8b9fd80d075148d92ebef4e47f458aa8365132c8
Szymon Lisiecki <szymon.lisiecki@likims.com>
2023-09-14 13:54:12 +0200

    sent lora commands to Kerlink

--- src/main/java/com/orange/lo/sample/kerlink2lo/kerlink/model/EndDeviceDto.java
+++ src/main/java/com/orange/lo/sample/kerlink2lo/kerlink/model/EndDeviceDto.java
@@ -8,11 +8,13 @@
 package com.orange.lo.sample.kerlink2lo.kerlink.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EndDeviceDto {
 
     @JsonProperty("devEui")
